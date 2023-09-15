ba7fa26f03f407f4a8ab51d4edf3c9624fca64e1
infinity-set <infinity-set>
2023-09-15 13:13:09 -0500

    Local commit

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-# port_scanner_ps
+# PowerShell Port Scanner
+
+## Overview
+
+Welcome to the PowerShell Port Scanner. This PowerShell script is designed to scan a range of ports on a target IP address or hostname and report which ports are open. It creates a scan report file with details of the scan and the open ports found.
+
+## Table of Contents
+
+- [Why Use This Script](#why-use-this-script)
+- [Functions](#functions)
+- [Dependencies and Permissions](#dependencies-and-permissions)
+- [Ethical Considerations](#ethical-considerations)
+- [Disclaimer](#disclaimer)
+- [How to Run](#how-to-run)
+- [Example Usage](#example-usage)
+
+## Why Use This Script
+
+Port scanning is a fundamental aspect of network and system administration, providing essential insights into the security and accessibility of networked devices. There are several reasons why you might find this script useful:
+
+- **Security Assessment:** Use this script to assess the security posture of your own network or systems. Identifying open ports can help you determine potential vulnerabilities and take proactive security measures.
+
+- **Network Troubleshooting:** When diagnosing network issues, knowing which ports are open or closed on a specific device can be invaluable. This script simplifies the process of checking port status.
+
+- **Inventory Management:** As part of inventory management, you can use this script to verify which services are running on your networked devices, aiding in resource allocation and maintenance.
+
+- **Educational Purposes:** If you're learning about network security or port scanning techniques, this script can serve as an educational tool to understand how port scanning works.
+
+- **Customization:** You can customize this script to suit your specific needs. Modify it to scan specific port ranges, automate routine port scans, or integrate it into your network monitoring tools.
+
+By using this script responsibly and ethically, you can enhance your network management and security practices while gaining valuable insights into your network's configuration and potential vulnerabilities.
+
+## Functions
+
+This script consists of three main functions:
+
+**Show-OpenPorts:** Displays the open ports on the target. If there are no open ports, it prints "None."
+
+**Create-ScanReport:** Creates a scan report file with date/time, scan details, and open ports (if any).
+
+**Test-PortRange:** Scans a range of ports on the target and logs the open ports. It utilizes the Create-ScanReport function for reporting.
+
+## Dependencies and Permissions
+
+This script relies on the following dependencies:
+
+- **PowerShell:** Ensure that you have PowerShell installed on your system. The script is designed to run in a PowerShell environment.
+
+**Permissions:** This script may require administrative privileges to run successfully, as it involves network communication and port scanning. Depending on your system's security settings, you might need to run PowerShell as an administrator and adjust the execution policy to allow script execution.
+
+## Ethical Considerations
+
+Port scanning can be subject to legal and ethical considerations. It's essential to use this script responsibly and ethically:
+
+- **Permission:** Ensure that you have proper authorization and permission to scan the target IP or hostname. Unauthorized scanning of systems or networks can be illegal and unethical.
+
+- **Ownership:** Respect the ownership of the scanned systems. Avoid scanning systems that you do not own or have explicit permission to scan.
+
+- **Privacy:** Be mindful of privacy concerns. Do not scan systems that may contain sensitive or private information without proper consent.
+
+- **Legal Compliance:** Comply with all applicable laws and regulations related to network scanning and cybersecurity. Laws governing port scanning may vary by jurisdiction.
+
+## Disclaimer
+
+This script is provided for educational and informational purposes only. The author and contributors of this script are not responsible for any unauthorized or unethical use of this tool. Users are solely responsible for ensuring they have the necessary permissions and compliance with all applicable laws when using this script.
+
+## How to Run
+
+1. **Download the Script:** Download the **_'port_scanner.ps1'_** script to your local machine.
+
+2. **Open PowerShell:** Open a PowerShell terminal.
+
+3. **Navigate to the Script Directory:** Use the **_'cd'_** command to navigate to the directory where port_scanner.ps1 is located.
+
+4. **Execute the Script:** Run the script by typing **_'.\port_scanner.ps1'_** and pressing Enter.
+
+5. **Follow the Prompts:** The script will prompt you to enter the target IP address or hostname and specify the port range for scanning. Follow the on-screen instructions.
+
+6. **View the Results:** After the scan is complete, the script will display open ports, save a report to a file, and provide the file path for viewing.
+
+   Note: Ensure that PowerShell execution policy allows script execution. You may need to run **_'Set-ExecutionPolicy RemoteSigned'_** to enable script execution.
+
+## Example Usage
+
+Here's an example of how to use the script:
+
+1. Execute the script as described in the "How to Run" section.
+
+2. Enter the target IP address or hostname.
+
+3. Enter the starting and ending port numbers for the scan range.
+
+4. Confirm the scan.
+
+The script will perform the port scan, display open ports (if any), and generate a scan report.
+
+You can view the scan report in the specified file path.
---
+++ port_scanner.ps1
@@ -0,0 +1,184 @@
+﻿# Function to display open ports
+function Show-OpenPorts {
+    param (
+        [array]$open_ports
+    )
+
+    Write-Host "Open Ports:"
+    if ($open_ports.Count -eq 0) {
+        Write-Host "None"
+    } else {
+        foreach ($port in $open_ports) {
+            Write-Host $port
+        }
+    }
+}
+
+# Define a function to create a scan report file.
+function Create-ScanReport {
+    param (
+        [string]$target,
+        [int]$startingPort,
+        [int]$endingPort
+    )
+
+    # Get the current date and time
+    $current_date = Get-Date 
+
+    # Define the filename based on the target
+    $filename = "portscan_${target}.txt"
+
+    # Get the current directory
+    $current_directory = Get-Location
+
+    # Combine the current directory and filename to create the file path
+    $FilePath = Join-Path -Path $current_directory -ChildPath $filename
+
+    # Create an empty file at the specified path
+    Out-File $FilePath
+
+    # Check if the file was successfully created
+    if (Test-Path -Path $FilePath -PathType Leaf) {
+        # Display information about the report file
+        Write-Host ""
+        Write-Host "Report file path:"
+        $content = @"
+Date/Time: $current_date
+Scanned Port(s): $startingPort - $endingPort
+Hostname/IP: $target
+
+Open Ports:
+"@
+        Add-Content -Path $FilePath -Value $content
+        return $FilePath
+    } else {
+        Write-Host "The file '$FilePath' does not exist."
+    }
+}
+
+# Function to test a range of ports on a target
+function Test-PortRange {
+    param (
+        [string]$target,
+        [int]$startingPort,
+        [int]$endingPort
+    )
+
+    # Initialize an array to store open ports
+    $open_ports = @()
+
+    # Call the Create-ScanReport function and capture the result (file path) in a variable.
+    Create-ScanReport -target $target -startingPort $startingPort -endingPort $endingPort
+
+    # Display a message to indicate that the port scan is in progress.
+    Write-Host ""  
+    Write-Host "Port Scan in progress for '$target', please wait..."
+    Write-Host ""  
+    
+    # Check if the target can be reached before conducting the port scan.
+    if (Test-Connection -ComputerName $target -Count 1 -Quiet) {
+        for ($port = $startingPort; $port -le $endingPort; $port++) {
+            $tcpClient = New-Object System.Net.Sockets.TcpClient
+            Write-Host "Attempting port $port..."  # Display the port being checked.
+
+            # Define FilePath here
+            $filename = "portscan_${target}.txt"
+            $current_directory = Get-Location
+            $FilePath = Join-Path -Path $current_directory -ChildPath $filename
+
+            # Try to establish a connection to the target IP and port.
+            try {
+                $tcpClient.Connect($target, $port)
+            
+                # If a connection is established (port is open), close the connection and display a message.
+                if ($tcpClient.Connected) {
+                    $tcpClient.Close()
+
+                    Write-Host "***********"
+                    Write-Host "Port: $port - Open" -BackgroundColor Black -ForegroundColor Green  # Indicate that the port is open.
+                    Write-Host "***********"
+                    $filename = "portscan_${target}.txt"
+                    $current_directory = Get-Location
+                    $FilePath = Join-Path -Path $current_directory -ChildPath $filename
+                    $open_ports += $port
+
+                    if ($FilePath) {
+                        Add-Content -Path $FilePath -Value "$port" 
+                    } 
+                }       
+            } catch {
+                # Ignore any connection errors.
+            }
+        }
+    
+        Write-Host ""
+        Write-Host "Port scan complete!"  # Indicate that the port scan is finished.
+        Write-Host "" 
+        Show-OpenPorts -open_ports $open_ports
+
+    } else {
+        Write-Host "The address '$target' is invalid or unreachable."
+        Write-Host "Exiting..."
+        exit
+    }  
+    if ($FilePath) {
+        Write-Host ""
+        Write-Host "View saved report file at: '$FilePath"
+        Write-Host ""
+    }
+}
+
+# Define the target IP address.
+Write-Host "" 
+$target = Read-Host "Enter a website or IP Address of the target"
+
+# Prompt the user to enter a positive starting port number.
+$startingPort = -1  # Initialize with an invalid value.
+while ($startingPort -lt 0) {
+    Write-Host "" 
+    $startingPort = Read-Host "Enter a positive starting port number"
+
+    # Check if the input is not empty and consists of digits only.
+    if (-not [string]::IsNullOrEmpty($startingPort) -and $startingPort -match '^\d+$') {
+        $startingPort = [int]$startingPort  # Convert the input to an integer.
+    } else {
+        Write-Host "" 
+        Write-Host "Invalid input. Please enter a positive integer."
+        $startingPort = -1  # Set to an invalid value to continue the loop.
+    }
+}
+
+# Prompt the user to enter a positive ending port number greater than the starting port.
+$endingPort = -1  # Initialize with an invalid value.
+while ($endingPort -lt $startingPort) {
+    Write-Host "" 
+    $endingPort = Read-Host "Enter a positive ending port number and greater than or equal to port $startingPort"
+
+    # Check if the input is not empty and consists of digits only.
+    if (-not [string]::IsNullOrEmpty($endingPort) -and $endingPort -match '^\d+$') {
+        $endingPort = [int]$endingPort  # Convert the input to an integer.
+    } else {
+        Write-Host "" 
+        Write-Host "Invalid input. Please enter a positive integer."
+        $endingPort = -1  # Set to an invalid value to continue the loop.
+    }
+}
+
+while ($true) {
+    Write-Host ""
+    Write-Host "Conduct port scan on '$target', port(s) $startingPort - $endingPort"
+    $input = Read-Host "Confirm: (Y) Yes or (N) No"
+    switch ($input){
+        "Y" {
+        Test-PortRange -target $target -startingPort $startingPort -endingPort $endingPort
+        exit
+        } 
+        "N" {
+        exit
+        }
+        default {
+        Write-Host ""
+        Write-Host "Please type 'Y' for YES or 'N' for NO"
+        } 
+    }
+}
